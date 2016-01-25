0f4cb31469bee3edc9c45885ba41a645f3f6b51b
Francescu Santoni <francescu@stupeflix.com>
2016-01-25 22:48:10 +0100

    Basic Epoch webserver

---
+++ Package.swift
@@ -0,0 +1,12 @@
+
+import PackageDescription
+
+let package = Package(
+    name: "Golb",
+    dependencies: [
+        .Package(url: "https://github.com/Zewo/Epoch.git", majorVersion: 0),
+        .Package(url: "https://github.com/Zewo/Middleware.git", majorVersion: 0),
+        .Package(url: "https://github.com/Zewo/PostgreSQL.git", majorVersion: 0),
+        .Package(url: "https://github.com/Zewo/Sideburns.git", majorVersion: 0),
+        ]
+        )
---
+++ Server/main.swift
@@ -0,0 +1,34 @@
+#if os(Linux)
+    import Glibc
+#else
+    import Darwin.C
+#endif
+import HTTP
+import Router
+import Epoch
+import CHTTPParser
+import CLibvenice
+import Sideburns
+
+let router = Router { router in
+    router.post("/users") { request in
+        // do something based on the Request
+        return Response(status: .Created)
+    }
+
+    router.get("/users/:id") { request in
+        let id = request.parameters["id"]
+        // do something based on the Request and id
+        return Response(status: .OK, body: "YYY")
+    } 
+
+    router.get("/:path") { request in
+        let data = ["path" : request.parameters["path"]]
+        return try! Response(status: .NotFound, templatePath: "./Client/404.html", templateData: data)
+    }
+
+
+}
+
+let server = Server(port: 8080, responder: router)
+server.start()
