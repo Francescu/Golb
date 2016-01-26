a00416405c298aff3bae5448be846e23f73a4b30
Francescu Santoni <francescu@stupeflix.com>
2016-01-26 21:51:48 +0100

    Added proper 404 fallback

--- Server/main.swift
+++ Server/main.swift
@@ -10,7 +10,8 @@ import CHTTPParser
 import CLibvenice
 import Sideburns
 
-let router = Router { router in
+let router = Router() { router in
+
     router.post("/users") { request in
         // do something based on the Request
         return Response(status: .Created)
@@ -22,9 +23,9 @@ let router = Router { router in
         return Response(status: .OK, body: "YYY")
     } 
 
-    router.get("/:path") { request in
+    router.fallback() { request in
         let data = ["path" : request.parameters["path"]]
-        return try! Response(status: .NotFound, templatePath: "./Client/404.html", templateData: data)
+        return Response(status: .NotFound, templatePath: "./Client/404.html", templateData: data)
     }
 
 
